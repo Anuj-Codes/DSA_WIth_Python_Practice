6f33e97ee5023ff65c9d40846d13ec3dd4607faf
Anuj-Codes <anuj.shrivastav24@gmail.com>
2023-12-29 14:14:11 +0530

    added delete item method

--- circular_linked_list.py
+++ circular_linked_list.py
@@ -77,6 +77,30 @@ class Cll:
                 temp.next=self.last.next
                 self.last=temp
         return "list is empty"
+    
+    def delete_item(self,data):
+        if not self.is_empty():
+            if self.last.next==self.last:
+                if self.last.item==data:
+                    self.last=None
+        else:
+            if self.last.next.item==data:
+                self.delete_first()
+            else:
+                temp=self.last.next
+                while temp!=self.last:
+                    if temp.next==self.last:
+                        self.delete_last()
+                        break
+                    if temp.next.item==data:
+                        temp.next=temp.next.next
+                        break
+                    temp=temp.next
+                return "data not found"
+
+
+            
+
         
 
         
