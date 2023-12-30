3188bb4b3b60c7f8da7a22274458d89ac2b4cfd6
Anuj-Codes <anuj.shrivastav24@gmail.com>
2023-12-30 13:55:06 +0530

    stack implimentation using list class extension

---
+++ stack.list.py
@@ -0,0 +1,31 @@
+class stack(list):
+    def is_empty(self):
+        return len(self)==0
+    
+    def push(self,data):
+        self.append(data)
+        
+    def pop(self):
+        if not self.is_empty():
+            return f"popped element is {super().pop()}"
+        else:
+            raise IndexError("stack is empty")
+    
+    def peek(self):
+        if not self.is_empty():
+            return self[-1]
+        else:
+            raise IndexError("stack is empty")
+    
+    def insert(self,index,data):
+        raise AttributeError("atrribute error 'insert' ")
+    
+
+s1=stack()
+s1.push(10)
+s1.push(20)
+print(s1.pop())
+print(s1.peek())
+s1.insert(1,60)
+        
+        
